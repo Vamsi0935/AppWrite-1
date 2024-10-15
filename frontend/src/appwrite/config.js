6faeca18d8e031f4881b9c6cb5c673ab556032ed
Vamsikrishna <141436747+Vamsi0935@users.noreply.github.com>
2024-10-15 13:37:58 +0530

    Update config.js

--- frontend/src/appwrite/config.js
+++ frontend/src/appwrite/config.js
@@ -3,7 +3,7 @@ import { Client, Account, Databases } from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint('https://app-write-1.vercel.app')
     .setProject('670c98b30011dd2ae8c2');
 
 export const account = new Account(client);
